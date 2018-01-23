777c3257645c6959e3abd9e5ff085e7310a2f21a
Unknown <annettebean@gmail.com>
2018-01-22 22:14:41 -0700

    Annette Initial version

---
+++ dgm1600/Assign 1/color.js
@@ -0,0 +1,10 @@
+/*jslint devel: true */
+
+var firstName, lastName, favColor;
+
+firstName = prompt('enter your first name ');
+lastName = prompt('enter your last name');
+favColor = prompt('What\'s your favorite color?');
+
+
+alert(firstName + ' ' + lastName + ' favorite color is ' + favColor);
---
+++ dgm1600/testprompt/prompt.js
@@ -0,0 +1,6 @@
+/*jslint devel: true*/
+
+
+var myColor = prompt("what is your favorite color? ");
+
+alert("your favorite color is: " + myColor);
